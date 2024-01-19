e9618902a0c2da4c0b349d161e5ceb339039c560
isa123pou <135288093+isa123pou@users.noreply.github.com>
2024-01-19 12:49:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+function preload() {
+
+}
+
+function setup() {
+  canvas = createCanvas(600, 600);
+  canvas.center();
+}
+
+function modelLoaded() {
+
+}
+
+function gotPoses(results)
+{
+ 
+  
+}
+
+function draw() {
+  
+ 
+}
+
+function take_snapshot(){    
+save('myFilterImage.png')
+}
